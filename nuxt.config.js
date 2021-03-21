1b2ecade6fe1a5d3f41d42d0a63c4839c65123d0
Tobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2021-03-21 00:37:55 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,10 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    'vue-scrollto/nuxt',
+
+        // Or if you have custom options...
+        ['vue-scrollto/nuxt', { duration: 300 }],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
