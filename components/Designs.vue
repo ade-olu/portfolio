dd1bd1bf3330f59081d2853f736da322ad74dc6e
Tobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2021-03-25 18:56:49 -0400

    Update Designs.vue

--- components/Designs.vue
+++ components/Designs.vue
@@ -17,7 +17,7 @@
         <div class="relative bg-orange rounded-md grid justify-center items-center row-start-1 row-end-8 small:row-end-9 tab:row-end-11 large:row-end-6 xlarge:row-end-7">
           <img class="ml-4 w-xt3 small:w-28 phone:w-32 tab:w-ff3 xlarge:w-ff4" src="../assets/designs/findfont.svg" alt="FindFont">
           <!-- Overlay -->
-           <div class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
+           <nuxt-link to="/designs/findfont" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
              <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
             <div class="z-20 mb-7 tab:mb-10 large:mb-9">
@@ -29,7 +29,7 @@
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
             </div>
-          </div>
+           </nuxt-link>
            <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
         </div>
 
@@ -37,7 +37,7 @@
         <div class="relative bg-travu rounded-md grid justify-center items-center row-start-8 row-end-15 small:row-start-9 small:row-end-17 tab:row-start-11 tab:row-end-21 large:row-start-1 large:row-end-8 xlarge:row-end-9">
           <img class="w-24 small:w-xt3 phone:w-ff tab:w-36 xlarge:w-44" src="../assets/designs/travu.svg" alt="Travu">
           <!-- Overlay -->
-           <div class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
+           <nuxt-link to="/designs/travu" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
              <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
             <div class="z-20 mb-7 tab:mb-10 large:mb-9">
@@ -49,7 +49,7 @@
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
             </div>
-          </div>
+           </nuxt-link>
            <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
         </div>
 
@@ -57,7 +57,7 @@
         <div class="relative bg-venzone rounded-md grid justify-center items-center row-start-15 row-end-22 small:row-start-17 small:row-end-25 tab:row-start-21 tab:row-end-31 large:col-start-1 large:col-end-2 large:row-start-6 large:row-end-13 xlarge:row-start-7 xlarge:row-end-15">
           <img class="w-24 small:w-xt3 phone:w-ff tab:w-36 xlarge:w-44 z-10" src="../assets/designs/venzone.svg" alt="VenZone">
           <!-- Overlay -->
-           <div class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
+           <nuxt-link to="/designs/venzone" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
              <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
             <div class="z-20 mb-7 tab:mb-10 large:mb-9">
@@ -69,7 +69,7 @@
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
             </div>
-          </div>
+           </nuxt-link>
            <img class="absolute w-full h-full" src="../assets/noise.png" alt="noise">
         </div>
 
@@ -77,7 +77,7 @@
         <div class="relative bg-ear1 rounded-md grid justify-center items-center row-start-22 row-end-29 small:row-start-25 small:row-end-33 tab:row-start-31 tab:row-end-41 large:col-start-2 large:col-end-3 large:row-start-8 large:row-end-13 xlarge:row-start-9 xlarge:row-end-15">
           <img class="w-24 small:w-xt3 phone:w-ff tab:w-36 xlarge:w-44" src="../assets/designs/ear1.svg" alt="Ear1">
           <!-- Overlay -->
-           <div class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
+           <nuxt-link to="/designs/ear1" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
              <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
             <div class="z-20 mb-7 tab:mb-10 large:mb-9">
@@ -89,7 +89,7 @@
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
             </div>
-          </div>
+           </nuxt-link>
            <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
         </div>
       </div>
