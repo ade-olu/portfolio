7a96c4124d9cc2b0138efc93acbae10035b4752d
Tobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2021-03-26 18:11:54 -0400

    Update Designs.vue

--- components/Designs.vue
+++ components/Designs.vue
@@ -17,40 +17,44 @@
         <div class="relative bg-orange rounded-md grid justify-center items-center row-start-1 row-end-8 small:row-end-9 tab:row-end-11 large:row-end-6 xlarge:row-end-7">
           <img class="ml-4 w-xt3 small:w-28 phone:w-32 tab:w-ff3 xlarge:w-ff4" src="../assets/designs/findfont.svg" alt="FindFont">
           <!-- Overlay -->
-           <nuxt-link to="/designs/findfont" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
-             <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
+           <nuxt-link to="/designs/findfont" class="overlay z-20 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
+             <div class="overlay-inner w-full h-full">
+             <div class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
-            <div class="z-20 mb-7 tab:mb-10 large:mb-9">
+            <div class="mb-7 tab:mb-10 large:mb-9">
             <h1 class="font-bold text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl">FindFont</h1>
             <p class=" text-grey3 mt-2 phone:text-lg tab:mt-5 tab:text-xl large:text-xl large:mt-4">An app design for finding typeface names on images.</p>
              </div>
             <!-- Technologies -->
-            <div class="z-20">
+            <div>
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
+             </div>
+            <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
             </div>
            </nuxt-link>
-           <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
         </div>
 
         <!-- Travu -->
         <div class="relative bg-travu rounded-md grid justify-center items-center row-start-8 row-end-15 small:row-start-9 small:row-end-17 tab:row-start-11 tab:row-end-21 large:row-start-1 large:row-end-8 xlarge:row-end-9">
           <img class="w-24 small:w-xt3 phone:w-ff tab:w-36 xlarge:w-44" src="../assets/designs/travu.svg" alt="Travu">
           <!-- Overlay -->
-           <nuxt-link to="/designs/travu" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
-             <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
+           <nuxt-link to="/designs/travu" class="overlay z-20 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
+             <div class="overlay-inner w-full h-full">
+             <div class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
-            <div class="z-20 mb-7 tab:mb-10 large:mb-9">
+            <div class="mb-7 tab:mb-10 large:mb-9">
             <h1 class="font-bold text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl">Travu (WIP)</h1>
             <p class=" text-grey3 mt-2 phone:text-lg tab:mt-5 tab:text-xl large:text-xl large:mt-4">A design for a travel and booking website.</p>
              </div>
             <!-- Technologies -->
-            <div class="z-20">
+            <div>
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
+             </div>
+             <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
             </div>
            </nuxt-link>
-           <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
         </div>
 
          <!-- VenZone -->
@@ -58,19 +62,21 @@
           <img class="w-24 small:w-xt3 phone:w-ff tab:w-36 xlarge:w-44 z-10" src="../assets/designs/venzone.svg" alt="VenZone">
           <!-- Overlay -->
            <nuxt-link to="/designs/venzone" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
-             <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
+             <div class="overlay-inner w-full h-full">
+             <div class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
-            <div class="z-20 mb-7 tab:mb-10 large:mb-9">
+            <div class="mb-7 tab:mb-10 large:mb-9">
             <h1 class="font-bold text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl">VenZone</h1>
             <p class=" text-grey3 mt-2 phone:text-lg tab:mt-5 tab:text-xl large:text-xl large:mt-4">A website design for selling food online.</p>
              </div>
             <!-- Technologies -->
-            <div class="z-20">
+            <div>
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
+             </div>
+             <img class="absolute w-full h-full" src="../assets/noise.png" alt="noise">
             </div>
            </nuxt-link>
-           <img class="absolute w-full h-full" src="../assets/noise.png" alt="noise">
         </div>
 
         <!-- Ear1 -->
@@ -78,20 +84,23 @@
           <img class="w-24 small:w-xt3 phone:w-ff tab:w-36 xlarge:w-44" src="../assets/designs/ear1.svg" alt="Ear1">
           <!-- Overlay -->
            <nuxt-link to="/designs/ear1" class="overlay z-10 bg-gradient-to-t from-bl to-bl2 rounded-md absolute w-full h-full xlarge:opacity-0 xlarge:transition-all xlarge:duration-200 hover:opacity-100">
-             <div class="overlay-inner absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
+             <div class="overlay-inner w-full h-full">
+             <div class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full px-5 pb-8 small:px-6 small:pb-8 tab:pb-12 tab:px-10 large:pb-12 large:px-10">
             <!-- Overlay Title & Subtitle -->
-            <div class="z-20 mb-7 tab:mb-10 large:mb-9">
+            <div class="mb-7 tab:mb-10 large:mb-9">
             <h1 class="font-bold text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl">Ear1</h1>
             <p class=" text-grey3 mt-2 phone:text-lg tab:mt-5 tab:text-xl large:text-xl large:mt-4">A website design concept for Ear1.</p>
              </div>
             <!-- Technologies -->
-            <div class="z-20">
+            <div>
             <span class="font-bold bg-grey2 text-white rounded-full w-auto px-6 py-2 phone:text-lg tab:text-xl large:text-xl large:px-8 large:py-2.5">Figma</span>
             </div>
             </div>
+            <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
+            </div>
            </nuxt-link>
-           <img class="absolute w-full h-full" src="../assets/bg.png" alt="Bg">
         </div>
+        
       </div>
       
      </div>
