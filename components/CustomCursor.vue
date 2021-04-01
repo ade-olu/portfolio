92f7fbe1053bdc12f5a564b259530fde0f3831bc
Tobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2021-03-31 22:21:08 -0400

    Update CustomCursor.vue

--- components/CustomCursor.vue
+++ components/CustomCursor.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="custom-cursor">
-    <div class="custom-cursor__circle" :style="circleStyle" ref="customCursorCircle"></div>
-    <div class="custom-cursor__dot" :style="dotStyle" ref="customCursorDot"></div>
+    <div
+      class="custom-cursor__circle"
+      :style="circleStyle"
+      ref="customCursorCircle"
+    ></div>
+    <div
+      class="custom-cursor__dot"
+      :style="dotStyle"
+      ref="customCursorDot"
+    ></div>
   </div>
 </template>
 
@@ -14,7 +22,7 @@ export default {
     circleColorHover: String,
     dotColor: String,
     dotColorHover: String,
-    hoverSize: Number
+    hoverSize: Number,
   },
   data() {
     return {
@@ -26,7 +34,7 @@ export default {
       dotPosX: null,
       dotPosY: null,
       circleStyle: null,
-      dotStyle: null
+      dotStyle: null,
     };
   },
   methods: {
@@ -63,46 +71,46 @@ export default {
       // Move Custom Cursor
       circle.style.transform = `translate(${this.circlePosX}px,${this.circlePosY}px) scale(${this.scale})`;
       dot.style.transform = `translate(${this.dotPosX}px,${this.dotPosY}px)`;
-    }
+    },
   },
   mounted() {
     window.addEventListener("mousemove", this.customCursor);
-  }
+  },
 };
 </script>
 
 <style scoped>
 .custom-cursor {
-	 cursor: none;
-	 pointer-events: none;
-   display: none;
-   z-index: 50 !important;
+  cursor: none;
+  pointer-events: none;
+  display: none;
+  z-index: 50 !important;
 }
- .custom-cursor__circle {
-	 position: fixed;
-	 cursor: none;
-	 top: 0;
-	 left: 0;
-	 width: 60px;
-	 height: 60px;
-	 border: 2px solid #0F1221;
-	 border-radius: 50%;
-	 transform: translate(-100%, -100%);
-	 transition: all 0.4s cubic-bezier(0.23, 1, 0.32, 1);
-   z-index: 50 !important;
+.custom-cursor__circle {
+  position: fixed;
+  cursor: none;
+  top: 0;
+  left: 0;
+  width: 60px;
+  height: 60px;
+  border: 2px solid #0f1221;
+  border-radius: 50%;
+  transform: translate(-100%, -100%);
+  transition: all 0.4s cubic-bezier(0.23, 1, 0.32, 1);
+  z-index: 50 !important;
 }
- .custom-cursor__dot {
-	 position: fixed;
-	 cursor: none;
-	 top: 1px;
-	 left: 1px;
-	 width: 15px;
-	 height: 15px;
-	 border-radius: 50%;
-	 background-color: #0F1221;
-	 transform: translate(-100%, -100%);
-	 transition: all 0.2s cubic-bezier(0.23, 1, 0.32, 1);
-   z-index: 50 !important;
+.custom-cursor__dot {
+  position: fixed;
+  cursor: none;
+  top: 1px;
+  left: 1px;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: #0f1221;
+  transform: translate(-100%, -100%);
+  transition: all 0.2s cubic-bezier(0.23, 1, 0.32, 1);
+  z-index: 50 !important;
 }
 
 @media screen and (min-width: 1025px) {
