bd93a9af0f1c38e55277060a8fc832cabf62182f
Tobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2021-05-10 21:48:16 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="relative container font-body bg-white">
+  <div
+    class="relative container font-body bg-white"
+    :class="{ stop: isActive }"
+  >
+    <!-- For Screens Lower Than 769px -->
+    <!-- Nav Menu -->
+    <transition name="fade" v-on:enter="enter">
+      <div
+        class="nav-menu-2 fixed w-full h-screen bg-light z-30 flex flex-col items-center justify-center large:hidden"
+        v-if="isActive"
+        id="nMenu"
+      >
+        <ul
+          class="nav-links phone:text-lg flex flex-col items-center justify-center text-black"
+          id="nLinks"
+        >
+          <li class="my-6">
+            <a href="#projects" @click="close"
+              ><span><hover>Projects</hover></span></a
+            >
+          </li>
+          <li class="my-6">
+            <a href="#"
+              ><span><hover>Designs</hover></span></a
+            >
+          </li>
+          <li class="my-6">
+            <a href="#"
+              ><span><hover>Resume</hover></span></a
+            >
+          </li>
+          <li class="my-6">
+            <a href="#contact" @click="close"
+              ><span><hover>Contact</hover></span></a
+            >
+          </li>
+        </ul>
+      </div>
+    </transition>
+
     <!-- Navbar -->
     <nav class="bg-white z-20" id="nav">
       <div
-        class="xlarge:absolute z-20 w-full h-ht flex items-center py-5 px-5 tab:h-32 tab:px-10 tab:justify-between large:px-logo large:h-36 xlarge:px-20"
+        class="xlarge:absolute z-20 w-full h-ht flex items-center py-5 px-5 justify-between tab:h-32 tab:px-10 large:px-logo large:h-36 xlarge:px-20"
       >
         <!-- Logo -->
-        <a href="/">
+        <a href="/" class="z-40">
           <img
             class="w-wt phone:w-sz tab:w-sz2 large:w-sz2"
             src="../assets/logo.svg"
@@ -14,6 +53,7 @@
           />
         </a>
 
+        <!-- For Screens Higher Than 768px -->
         <!-- Nav Menu -->
         <div class="nav-menu hidden large:block" id="nMenu">
           <ul
@@ -41,6 +81,16 @@
               >
             </li>
           </ul>
+        </div>
+
+        <!-- For Screens Lower Than 769px -->
+        <!-- Hamburger Menu -->
+        <div class="flex item-center relative w-10 z-40 mr-0.5 large:hidden">
+          <div
+            class="hamburger-menu"
+            @click="menu"
+            :class="{ animate: isActive }"
+          ></div>
         </div>
       </div>
     </nav>
@@ -250,6 +300,26 @@ export default {
   components: {
     Projects,
     Designs,
+  },
+
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+
+  methods: {
+    menu: function() {
+      this.isActive = !this.isActive;
+    },
+
+    enter: function(el, done) {
+      var that = this;
+    },
+
+    close: function() {
+      this.isActive = !this.isActive;
+    },
   },
 };
 </script>
