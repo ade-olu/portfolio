e0c5fb19b2e88848e37e7f9d52263482897d8d32
Tobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2021-10-17 17:49:13 -0400

    Update works.vue

--- pages/designs/works.vue
+++ pages/designs/works.vue
@@ -15,7 +15,7 @@
       <!-- Heading -->
       <div class="text-center">
         <h1
-          class="font-body font-bold text-3xl text-black leading-10 mt-4 phone:text-fsize phone:leading-pht xlarge:mt-2 tab:text-tfsize tab:leading-lht large:text-lfsize large:leading-lglht xlarge:text-xlfsize xlarge:leading-xlglht 2xlarge:text-2xlfsize"
+          class="font-head font-bold text-3xl text-black leading-10 mt-4 phone:text-fsize phone:leading-pht xlarge:mt-2 tab:text-tfsize tab:leading-lht large:text-lfsize large:leading-lglht xlarge:text-xlfsize xlarge:leading-xlglht 2xlarge:text-2xlfsize"
         >
           <span class="relative z-10">Designs</span>
           <span
@@ -85,7 +85,7 @@
                 <!-- Overlay Title & Subtitle -->
                 <div class="mb-5 tab:mb-9 large:mb-7 xlarge:mb-9">
                   <h1
-                    class="font-bold text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl xlarge:text-ofsize"
+                    class="font-head text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl xlarge:text-ofsize"
                   >
                     FindFont
                   </h1>
@@ -129,7 +129,7 @@
                 <!-- Overlay Title & Subtitle -->
                 <div class="mb-5 tab:mb-9 large:mb-7 xlarge:mb-9">
                   <h1
-                    class="font-bold text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl xlarge:text-ofsize"
+                    class="font-head text-white text-2xl phone:text-fsz tab:text-4xl large:text-4xl xlarge:text-ofsize"
                   >
                     Ear1
                   </h1>
