5331632742cd49e4a85c7e57ffe84978ada78425
Oluwatobiloba Adegbaju <adegbajuoezekiel@gmail.com>
2026-01-23 17:48:50 -0500

    refactor(components): migrate Designs to Vue 3 Composition API

--- components/Designs.vue
+++ components/Designs.vue
@@ -1,346 +1,250 @@
 <template>
-
   <!-- Designs -->
-
   <div
-    class="mt-2 pt-10 pb-14 bg-white-soft relative w-full h-auto phone-sm:mt-4 tablet-xs:mt-10 tablet-xs:pt-10 tablet-xs:pb-14 lg:mt-8 lg:pt-14 lg:pb-24 xl:mt-14"
     id="designs"
+    class="mt-2 pt-10 pb-14 bg-white-soft relative w-full h-auto phone-sm:mt-4 tablet-xs:mt-10 tablet-xs:pt-10 tablet-xs:pb-14 lg:mt-8 lg:pt-14 lg:pb-24 xl:mt-14"
   >
-
     <div class="flex items-center justify-center">
-
       <div class="text-center overflow-hidden">
-
         <!-- Heading -->
-
         <h1
           class="quote font-heading font-bold text-3xl text-black-primary leading-10 phone-sm:text-heading-1 phone-sm:leading-tight tablet-xs:text-heading-2 tablet-xs:leading-normal lg:text-heading-4 lg:leading-loose xl:text-heading-5 2xl:text-heading-6"
         >
-
           <span class="relative z-10">Designs</span>
-
           <span
             class="relative flex justify-center items-center -mt-4 z-0 phone-sm:-mt-5 lg:-mt-5"
             data-cursor-hover
           >
-
             <img
               class="w-32 phone-sm:w-36 tablet-xs:w-44 lg:w-48 xl:w-56"
               src="../assets/accent-line.svg"
               alt="Accent Line"
             />
-
           </span>
-
         </h1>
-
       </div>
-
     </div>
 
     <div
       class="grid grid-col-1 px-8 gap-14 mt-11 phone-sm:mt-12 tablet-xs:px-14 tablet-xs:gap-14 lg:mt-16 lg:px-24 lg:grid-cols-2 lg:gap-24"
     >
-
       <!-- TalkEase -->
-
       <div
         class="relative bg-talkease rounded-md grid justify-center items-center row-span-6 xs:row-span-7 phone-sm:row-span-8 tablet-xs:row-span-10 lg:row-span-5 xl:row-span-6"
       >
-
         <img
           class="w-24 xs:w-width-11 phone-sm:w-width-12 tablet-xs:w-36 xl:w-44"
           src="../assets/works/talkease.svg"
           alt="TalkEase"
         />
-
-        <nuxt-link
+        <NuxtLink
           to="/works/talkease"
           data-cursor-hover
           class="overlay z-20 bg-gradient-to-t from-black-opacity-50 to-black-opacity-10 rounded-md absolute w-full h-full xl:opacity-0 xl:transition-all xl:duration-200 hover:opacity-100"
         >
-
           <div class="overlay-inner w-full h-full">
-
             <div
               class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full p-6 xs:p-6 tablet-xs:p-10"
             >
-
               <div class="mb-5 tablet-xs:mb-9 lg:mb-7 xl:mb-9">
-
                 <h1
                   class="font-heading text-white-soft text-2xl phone-sm:text-display tablet-xs:text-4xl lg:text-4xl xl:text-heading-3"
                 >
-                   TalkEase
+                  TalkEase
                 </h1>
-
                 <p
                   class="text-gray-light mt-2 phone-sm:text-lg tablet-xs:mt-5 tablet-xs:text-xl lg:text-xl lg:mt-4 xl:mt-5"
                 >
-                   An app design for real-time multilingual communication using AI
-                  translation.
+                  An app design for real-time multilingual communication using
+                  AI translation.
                 </p>
-
               </div>
-
               <div class="flex flex-wrap gap-4 tablet-xs:gap-6">
-
                 <span
                   class="inline-flex items-center justify-center font-bold bg-gray text-white-soft rounded-full text-xs phone-sm:text-sm tablet-xs:text-base px-4 py-1.5 tablet-xs:px-6 tablet-xs:py-2 whitespace-nowrap"
                 >
-                   Figma
+                  Figma
                 </span>
-
               </div>
-
             </div>
-
           </div>
-
-        </nuxt-link>
-
+        </NuxtLink>
       </div>
 
       <!-- TripVoyager -->
-
       <div
         class="relative bg-tripvoyager rounded-md grid justify-center items-center row-span-6 xs:row-span-7 phone-sm:row-span-8 tablet-xs:row-span-10 lg:row-span-6 xl:row-span-8"
       >
-
         <img
           class="ml-4 w-width-11 xs:w-28 phone-sm:w-32 tablet-xs:w-width-14 xl:w-width-17"
           src="../assets/works/tripvoyager.svg"
           alt="TripVoyager"
         />
-
-        <nuxt-link
+        <NuxtLink
           to="/works/tripvoyager"
           data-cursor-hover
           class="overlay z-20 bg-gradient-to-t from-black-opacity-50 to-black-opacity-10 rounded-md absolute w-full h-full xl:opacity-0 xl:transition-all xl:duration-200 hover:opacity-100"
         >
-
           <div class="overlay-inner w-full h-full">
-
             <div
               class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full p-6 xs:p-6 tablet-xs:p-10"
             >
-
               <div class="mb-5 tablet-xs:mb-9 lg:mb-7 xl:mb-9">
-
                 <h1
                   class="font-heading text-white-soft text-2xl phone-sm:text-display tablet-xs:text-4xl lg:text-4xl xl:text-heading-3"
                 >
-                   TripVoyager
+                  TripVoyager
                 </h1>
-
                 <p
                   class="text-gray-light mt-2 phone-sm:text-lg tablet-xs:mt-5 tablet-xs:text-xl lg:text-xl lg:mt-4 xl:mt-5"
                 >
-                   An app design for planning vacations and trips.
+                  An app design for planning vacations and trips.
                 </p>
-
               </div>
-
               <div class="flex flex-wrap gap-4 tablet-xs:gap-6">
-
                 <span
                   class="inline-flex items-center justify-center font-bold bg-gray text-white-soft rounded-full text-xs phone-sm:text-sm tablet-xs:text-base px-4 py-1.5 tablet-xs:px-6 tablet-xs:py-2 whitespace-nowrap"
                 >
-                   Figma
+                  Figma
                 </span>
-
               </div>
-
             </div>
-
           </div>
-
-        </nuxt-link>
-
+        </NuxtLink>
       </div>
 
       <!-- FindFont -->
-
       <div
         class="relative bg-gradient-to-tr from-brand-orange to-brand-orange-light rounded-md grid justify-center items-center row-span-6 xs:row-span-7 phone-sm:row-span-8 tablet-xs:row-span-10 lg:row-span-6 xl:row-span-8"
       >
-
         <img
           class="ml-4 w-width-11 xs:w-28 phone-sm:w-32 tablet-xs:w-width-14 xl:w-width-17"
           src="../assets/works/findfont.svg"
           alt="FindFont"
         />
-
-        <nuxt-link
+        <NuxtLink
           to="/works/findfont"
           data-cursor-hover
           class="overlay z-20 bg-gradient-to-t from-black-opacity-50 to-black-opacity-10 rounded-md absolute w-full h-full xl:opacity-0 xl:transition-all xl:duration-200 hover:opacity-100"
         >
-
           <div class="overlay-inner w-full h-full">
-
             <div
               class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full p-6 xs:p-6 tablet-xs:p-10"
             >
-
               <div class="mb-5 tablet-xs:mb-9 lg:mb-7 xl:mb-9">
-
                 <h1
                   class="font-heading text-white-soft text-2xl phone-sm:text-display tablet-xs:text-4xl lg:text-4xl xl:text-heading-3"
                 >
-                   FindFont
+                  FindFont
                 </h1>
-
                 <p
                   class="text-gray-light mt-2 phone-sm:text-lg tablet-xs:mt-5 tablet-xs:text-xl lg:text-xl lg:mt-4 xl:mt-5"
                 >
-                   An app design for finding typeface names on images.
+                  An app design for finding typeface names on images.
                 </p>
-
               </div>
-
               <div class="flex flex-wrap gap-4 tablet-xs:gap-6">
-
                 <span
                   class="inline-flex items-center justify-center font-bold bg-gray text-white-soft rounded-full text-xs phone-sm:text-sm tablet-xs:text-base px-4 py-1.5 tablet-xs:px-6 tablet-xs:py-2 whitespace-nowrap"
                 >
-                   Figma
+                  Figma
                 </span>
-
               </div>
-
             </div>
-
           </div>
-
-        </nuxt-link>
-
+        </NuxtLink>
       </div>
 
       <!-- Ear1 -->
-
       <div
         class="relative bg-ear1 rounded-md grid justify-center items-center row-span-6 xs:row-span-7 phone-sm:row-span-8 tablet-xs:row-span-10 lg:row-span-5 xl:row-span-6"
       >
-
         <img
           class="w-24 xs:w-width-11 phone-sm:w-width-12 tablet-xs:w-36 xl:w-44"
           src="../assets/works/ear1.svg"
           alt="Ear1"
         />
-
-        <nuxt-link
+        <NuxtLink
           to="/works/ear1"
           data-cursor-hover
           class="overlay z-10 bg-gradient-to-t from-black-opacity-50 to-black-opacity-10 rounded-md absolute w-full h-full xl:opacity-0 xl:transition-all xl:duration-200 hover:opacity-100"
         >
-
           <div class="overlay-inner w-full h-full">
-
             <div
               class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full p-6 xs:p-6 tablet-xs:p-10"
             >
-
               <div class="mb-5 tablet-xs:mb-9 lg:mb-7 xl:mb-9">
-
                 <h1
                   class="font-heading text-white-soft text-2xl phone-sm:text-display tablet-xs:text-4xl lg:text-4xl xl:text-heading-3"
                 >
-                   Ear1
+                  Ear1
                 </h1>
-
                 <p
                   class="text-gray-light mt-2 phone-sm:text-lg tablet-xs:mt-5 tablet-xs:text-xl lg:text-xl lg:mt-4 xl:mt-5"
                 >
-                   A website design concept for Ear1.
+                  A website design concept for Ear1.
                 </p>
-
               </div>
-
               <div class="flex flex-wrap gap-4 tablet-xs:gap-6">
-
                 <span
                   class="inline-flex items-center justify-center font-bold bg-gray text-white-soft rounded-full text-xs phone-sm:text-sm tablet-xs:text-base px-4 py-1.5 tablet-xs:px-6 tablet-xs:py-2 whitespace-nowrap"
                 >
-                   Figma
+                  Figma
                 </span>
-
               </div>
-
             </div>
-
           </div>
-
-        </nuxt-link>
-
+        </NuxtLink>
       </div>
 
       <!-- GetFood -->
-
       <div
         class="relative bg-getfood rounded-md grid justify-center items-center row-span-6 xs:row-span-7 phone-sm:row-span-8 tablet-xs:row-span-10 lg:row-span-5 xl:row-span-6"
       >
-
         <img
           class="w-24 xs:w-width-11 phone-sm:w-width-12 tablet-xs:w-36 xl:w-44"
           src="../assets/works/getfood.svg"
           alt="GetFood"
         />
-
-        <nuxt-link
+        <NuxtLink
           to="/works/getfood"
           data-cursor-hover
           class="overlay z-10 bg-gradient-to-t from-black-opacity-50 to-black-opacity-10 rounded-md absolute w-full h-full xl:opacity-0 xl:transition-all xl:duration-200 hover:opacity-100"
         >
-
           <div class="overlay-inner w-full h-full">
-
             <div
               class="absolute flex flex-col justify-end rounded-md top-0 left-0 w-full h-full p-6 xs:p-6 tablet-xs:p-10"
             >
-
               <div class="mb-5 tablet-xs:mb-9 lg:mb-7 xl:mb-9">
-
                 <h1
                   class="font-heading text-white-soft text-2xl phone-sm:text-display tablet-xs:text-4xl lg:text-4xl xl:text-heading-3"
                 >
-                   GetFood
+                  GetFood
                 </h1>
-
                 <p
                   class="text-gray-light mt-2 phone-sm:text-lg tablet-xs:mt-5 tablet-xs:text-xl lg:text-xl lg:mt-4 xl:mt-5"
                 >
-                   An app design for ordering meals.
+                  An app design for ordering meals.
                 </p>
-
               </div>
-
               <div class="flex flex-wrap gap-4 tablet-xs:gap-6">
-
                 <span
                   class="inline-flex items-center justify-center font-bold bg-gray text-white-soft rounded-full text-xs phone-sm:text-sm tablet-xs:text-base px-4 py-1.5 tablet-xs:px-6 tablet-xs:py-2 whitespace-nowrap"
                 >
-                   Figma
+                  Figma
                 </span>
-
               </div>
-
             </div>
-
           </div>
-
-        </nuxt-link>
-
+        </NuxtLink>
       </div>
-
     </div>
-
   </div>
-
 </template>
 
-<script>
-export default {};
+<script setup>
+// No reactive data or methods needed for this component
+// Component is purely presentational
 </script>
 
 <style scoped>
@@ -355,4 +259,3 @@ export default {};
   }
 }
 </style>
-
